899b5f052dec344a6b63151958f3a4ca00acdd32
fyurchik <jyrafenun@gmail.com>
2024-01-27 17:56:48 +0200

    changed parser logic structure

--- lib/app.rb
+++ lib/app.rb
@@ -1,15 +1,3 @@
-require_relative 'models/vacancy'
 require_relative 'scraper'
-require_relative 'database'
-
-DataBase.connect
-
-unless ActiveRecord::Base.connection.table_exists?('vacancies')
-  CreateVacancies.new.change
-end
 
 Scraper.call
-
-Vacancy.all.each do |vacancy|
-  puts vacancy.title
-end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,18 @@
 require 'httparty'
 require 'nokogiri'
 require_relative '../config'
+require_relative 'models/vacancy'
+require_relative 'database'
 
 class Scraper
- 
   def self.call
-    scrape
+    DataBase.connect
+
+    unless ActiveRecord::Base.connection.table_exists?('vacancies')
+      CreateVacancies.new.change
+    end
+
+    save_in_db(scrape)
   end
 
   private
@@ -14,21 +21,31 @@ class Scraper
     start_url = URLS::START_URL
     response = HTTParty.get(start_url)
     doc = Nokogiri::HTML(response.body)
-  
+
     all_hrefs = doc.css('ul[aria-label="All teams roles"] li a').map { |link| link['href'] if link['href'].include?("careers")}
     all_hrefs.compact!
-    
+
+    scraped_vacancies = []
+
     all_hrefs.each do |href|
       new_url = URLS::BASE_URL + href
       response = HTTParty.get(new_url)
       doc = Nokogiri::HTML(response.body)
-  
+
       title = doc.css('.pt-spacing-9 h1').text
       location = doc.css('p.f-subhead-1.ui-richtext span').text
       url = doc.at('.lg\\:absolute.top-0.left-0.right-0.flex.flex-col a')['href']
       description = doc.at('.ui-description.ui-richtext').to_html
-  
-      Vacancy.new(title: title, location: location, url: url, description: description).save
+
+      scraped_vacancies << {title: title, location: location,url: url, description: description}
+    end
+
+    return scraped_vacancies
+  end
+
+  def self.save_in_db(scraped)
+    scraped.each do |vacancy|
+      Vacancy.new(vacancy).save
     end
   end
 end
