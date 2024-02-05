e8fafd0bd6b4572ef375f26edb4bb698589c993e
fyurchik <jyrafenun@gmail.com>
2024-02-05 13:22:18 +0200

    add rescue block for database save errors

--- lib/vacancy_database_handler.rb
+++ lib/vacancy_database_handler.rb
@@ -13,5 +13,7 @@ class VacancyDatabaseHandler
     @scraped_data.each do |vacancy|
       Vacancy.new(vacancy).save
     end
+  rescue StandardError => e
+    raise "Failed to save data: #{e.message}"
   end
 end
