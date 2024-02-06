d968ae3b16ab49312c26a0ffe12b7edbc594c259
fyurchik <jyrafenun@gmail.com>
2024-02-06 20:02:43 +0200

    Refactor RSpec tests for VacancyDatabaseHandler class

--- spec/vacancy_database_handler_spec.rb
+++ spec/vacancy_database_handler_spec.rb
@@ -1,25 +1,7 @@
-# spec/vacancy_database_handler_spec.rb
-
 require 'spec_helper'
 require_relative '../lib/vacancy_database_handler'
 
 RSpec.describe VacancyDatabaseHandler do
-  describe '#initialize' do
-    context 'when not raising an error' do 
-      let(:handler) { VacancyDatabaseHandler.new({title: "something", location: "somewhere"}) }
-      it 'initializes without errors' do
-        expect { handler }.to_not raise_error
-      end
-    end
-
-    context 'when raising an error'  do 
-      let(:handler) { VacancyDatabaseHandler.new }
-      it 'initializes with errors' do
-        expect { handler }.to raise_error(ArgumentError)
-      end
-    end 
-  end
-
   describe '#save_in_db' do
     let(:scraped_data) do
       [
@@ -29,30 +11,31 @@ RSpec.describe VacancyDatabaseHandler do
         { title: 'Data Scientist2', location: 'LA' }
       ]
     end
-    let(:handler) { VacancyDatabaseHandler.new(scraped_data) }
+
     it 'saves scraped data properly' do
-      vacancy_double = double('vacancy', save: true)
+      vacancy_double = instance_double('Vacancy', save: true)
 
       allow(Vacancy).to receive(:new).and_return(vacancy_double)
 
-      handler.save_in_db
+      subject.save_in_db(scraped_data)
 
       scraped_data.each do |vacancy_data|
         expect(Vacancy).to have_received(:new).with(vacancy_data)
       end
+
       expect(vacancy_double).to have_received(:save).exactly(scraped_data.length).times
     end
 
     context 'when saving to the database fails' do
-      let(:error_message) {'Failed to save to the database'}
-      
+      let(:error_message) { 'Failed to save to the database' }
+
       it 'raises an error' do
-        error_double = double('vacancy')
+        error_double = instance_double('vacancy')
         allow(error_double).to receive(:save).and_raise(RuntimeError, error_message)
 
         allow(Vacancy).to receive(:new).and_return(error_double)
 
-        expect { handler.save_in_db }.to raise_error(RuntimeError, "Failed to save data: #{error_message}")
+        expect { subject.save_in_db(scraped_data) }.to raise_error(RuntimeError, "Failed to save data: #{error_message}")
       end
     end
   end
